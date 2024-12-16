157998d89e0eeaffe429eea56dc09ca956594661
SomeTyraredGuy <iuriich2@gmail.com>
2024-12-16 22:24:09 +0000

    boards, members

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :boards
   root "pages#home"
   devise_for :user
   get "pricing" => "pages#pricing"
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_12_165900) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_16_220602) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "boards", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "members", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "board_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["board_id"], name: "index_members_on_board_id"
+    t.index ["user_id"], name: "index_members_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +41,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_12_165900) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "members", "boards"
+  add_foreign_key "members", "users"
 end
