eba3942743899a45ae08f6692ff55d07a90cfde7
SomeTyraredGuy <iuriich2@gmail.com>
2025-05-17 09:04:20 +0000

    board menu

--- app/javascript/components/Board/BoardMenu/BoardMenu.tsx
+++ app/javascript/components/Board/BoardMenu/BoardMenu.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { UndoSVG } from "../../svg/ToolsSVG";
-import SavingIcon from "../../General/SavingIcon";
+import SavingIcon from "../SavingIcon";
 import Hint from "@/components/General/Hint/Hint";
+import { useTranslation } from "react-i18next";
+import getMeta from "@/scripts/getMeta";
 
 type Props = {
 	boardName: string;
@@ -10,15 +12,16 @@ type Props = {
 };
 
 function BoardMenu({ boardName, unsavedChanges, showSaving }: Props) {
+	const { t } = useTranslation("translation", { keyPrefix: "board.board_menu" });
 	return (
 		<table className="w-2xs fixed top-0 m-2">
 			<tbody className="w-100">
 				<tr className="flex h-16 justify-end">
 					<th className="bg-background hover:bg-primary border-standard flex items-center rounded-l-2xl !border-r-0 p-0">
 						<Hint
-							title="Exit board"
+							title={t("exit")}
 							side="bottom"
-							href="/boards"
+							href={`/${getMeta("locale")}/boards`}
 							className="hover:text-secondary flex h-full w-12 items-center justify-center"
 						>
 							<UndoSVG className="h-6 w-6" />
--- app/javascript/components/Board/SavingIcon.tsx
+++ app/javascript/components/Board/SavingIcon.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import CheckSVG from "../svg/CheckSVG";
-import Hint from "./Hint/Hint";
+import Hint from "../General/Hint/Hint";
+import { useTranslation } from "react-i18next";
 
 type Props = {
 	unsavedChanges: boolean;
 };
 
 function SavingIcon({ unsavedChanges }: Props) {
+	const { t } = useTranslation("translation", { keyPrefix: "board.board_menu" });
 	return (
-		<Hint description={unsavedChanges ? "Saving..." : "Saved"} side="bottom">
+		<Hint description={unsavedChanges ? t("saving") : t("saved")} side="bottom">
 			{unsavedChanges ? (
 				<div className="border-t-primary h-8 w-8 animate-spin rounded-full border-4 border-gray-300"></div>
 			) : (
