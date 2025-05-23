189982df9688e224ffe70322d5ebb1e93c00f1f7
SomeTyraredGuy <iuriich2@gmail.com>
2025-05-23 13:23:23 +0000

    user controller tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = users(:Owner)
+    sign_in @user
+  end
+
+  test "should get current user" do
+    get current_user_url(I18n.locale, @board)
+
+    assert_response :success
+    assert_equal @user.id, JSON.parse(@response.body)["id"]
+  end
 end
